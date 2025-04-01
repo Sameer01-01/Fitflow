86f2c2c7167adc853a944afcb76a23c213f86688
Sameer <hisameer0026@gmail.com>
2025-04-01 23:42:44 +0530

    hehe

--- aifeatures/src/App.jsx
+++ aifeatures/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import PoseComponent from './component/PoseComponent'
-import DietPlanForm from './component/Dietplans'
-import Events from './component/Events'
+
 
 const App = () => {
   return (
